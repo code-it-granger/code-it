fb824387aa98d03a4a4483df2f307f1afd2dba00
code-it <codeitgranger@gmail.com>
2019-09-25 15:30:38 -0700

    LinkedList

---
+++ Code-It/Coding/src/linkedList/LinkedListDemo.java
@@ -0,0 +1,42 @@
+package linkedList;
+
+import linkedList.LinkedList.Node;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.head = linkedList.createDummyList();
+        linkedList.print();
+
+        kthLastElement(linkedList.head, 1);
+        middleElement(linkedList.head);
+    }
+
+    public static void kthLastElement(Node head, int k) {
+        if (head == null || k <= 0)
+            return;
+        Node slow = head, fast = head;
+        for (int i = 0; i < k; i++)
+            if (fast != null)
+                fast = fast.next;
+            else
+                return;
+
+        while (slow != null && fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        System.out.println(slow.data);
+    }
+
+    public static void middleElement(Node head) {
+        if (head == null)
+            return;
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println(slow.data);
+    }
+}
---
+++ Code-It/DataStructures/src/linkedList/singleLinkedList/LinkedList.java
@@ -0,0 +1,123 @@
+package linkedList.singleLinkedList;
+
+public class LinkedList {
+    static class Node{
+        Node next;
+        int data;
+
+        Node(int d){
+            this.data=d;
+        }
+    }
+
+    Node head;
+
+    public void insertAtHead(int d){
+        Node node = new Node(d);
+        if(head==null){
+            head=node;
+        }else{
+            node.next=head;
+            head=node;
+        }
+    }
+
+    public void insertAtTail(int d){
+        if(head==null)
+            insertAtHead(d);
+        else{
+            Node node=new Node(d);
+            Node  n=head;
+            while(n.next!=null)
+                n=n.next;
+            n.next=node;
+        }
+    }
+
+    public void deleteHead(){
+        if(head==null)
+            return;
+        if(head.next==null)
+            head=null;
+        else{
+            head.data=head.next.data;
+            head.next=head.next.next;
+        }
+    }
+
+    public void deleteTail(){
+        if(head==null)
+            return;
+        if(head.next==null)
+            head=null;
+        else{
+            Node n = head;
+            while(n.next.next!=null)
+                n=n.next;
+            n.next=null;
+        }
+    }
+
+    public void deleteNode(int d){
+        if(head==null)
+            return;
+        if(head.data==d){
+            if(head.next!=null) {
+                head.data = head.next.data;
+                head.next = head.next.next;
+            }
+            else
+                head=null;
+        }
+        Node n=head;
+        while(n.next!=null){
+            if(n.next.data==d) {
+                n.next = n.next.next;
+                return;
+            }
+            n=n.next;
+        }
+    }
+
+    public void reverse() {
+        Node current = head;
+        Node prev = null;
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head=prev;
+    }
+
+    public Node createDummyList(){
+        Node A=new Node(1);
+        Node B=new Node(3);
+        Node C=new Node(2);
+        Node D=new Node(4);
+        Node E=new Node(6);
+        Node F=new Node(5);
+
+        this.head=A;
+        A.next=B;
+        B.next=C;
+        C.next=D;
+        D.next=E;
+        E.next=F;
+
+        return this.head;
+    }
+
+    public void print(){
+        if(head==null)
+            return;
+        Node node = head;
+        while(node!=null){
+            System.out.print(node.data+" ");
+            node=node.next;
+        }
+        System.out.println();
+        return;
+    }
+}
